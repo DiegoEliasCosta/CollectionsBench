bf3c0d939ef176331994237ec9ee2a1f5a2e1680
Diego <diegoelias1@gmail.com>
2015-11-19 15:33:13 +0100

    Creating the structure for the map tests.

--- container-bench/src/main/java/de/heidelberg/pvs/container_bench/abstracts/AbstractListTest.java
+++ container-bench/src/main/java/de/heidelberg/pvs/container_bench/abstracts/AbstractListTest.java
@@ -1,38 +1,11 @@
 package de.heidelberg.pvs.container_bench.abstracts;
 
-import java.util.concurrent.TimeUnit;
-
-import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Fork;
-import org.openjdk.jmh.annotations.Measurement;
-import org.openjdk.jmh.annotations.Mode;
-import org.openjdk.jmh.annotations.OutputTimeUnit;
-import org.openjdk.jmh.annotations.Param;
-import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
-import org.openjdk.jmh.annotations.State;
-import org.openjdk.jmh.annotations.Threads;
-import org.openjdk.jmh.annotations.Warmup;
 import org.openjdk.jmh.infra.Blackhole;
 
-@BenchmarkMode(Mode.AverageTime)
-@OutputTimeUnit(TimeUnit.NANOSECONDS)
-@Warmup(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
-@Measurement(iterations = 10, time = 1, timeUnit = TimeUnit.SECONDS)
-@Threads(1)
-@Fork(2)
-@State(Scope.Benchmark)
-public abstract class AbstractListTest {
 
-	@Param({ "10000" })
-	public int size;
-
-	@Param({ "0" })
-	public int seed;
+public abstract class AbstractListTest extends AbstractBenchmarkTest { 
 	
-	@Setup
-	abstract public void setup(Blackhole blackhole);
-
 	/**
 	 * Benchmark GetAll
 	 * 
---
+++ container-bench/src/main/java/de/heidelberg/pvs/container_bench/abstracts/AbstractMapTests.java
@@ -0,0 +1,13 @@
+package de.heidelberg.pvs.container_bench.abstracts;
+
+public abstract class AbstractMapTests extends AbstractBenchmarkTest {
+	
+	
+	
+	abstract public void put(); 
+	
+	abstract public void contains();
+	
+	
+
+}
