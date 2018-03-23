5f247b8ab2e027a7c242a695814ea7c08441c19d
unknown <diegoelias1@gmail.com>
2018-03-23 17:28:26 +0100

    Adding the JDKcompute operation to wordcount bench

---
+++ src/main/java/de/heidelberg/pvs/container_bench/wordcount/JDK8Compute.java
@@ -0,0 +1,13 @@
+package de.heidelberg.pvs.container_bench.wordcount;
+
+import java.util.Map;
+
+public class JDK8Compute extends AbstractJDKCompatible {
+
+	@Override
+	protected void count(Map<Object, Integer> map, Object object) {
+		map.compute(object, (key, value) -> value == null ? 1 : value + 1);
+
+	}
+	
+}
