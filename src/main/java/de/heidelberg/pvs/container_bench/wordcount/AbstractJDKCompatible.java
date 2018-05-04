c7ccfb2f5b622db527f3a16e8e2df459f2bf50d0
Erich Schubert <schubert@informatik.uni-heidelberg.de>
2018-05-04 15:59:24 +0200

    Add Javolution, Koloboke QHash, Fastutil linked

--- src/main/java/de/heidelberg/pvs/container_bench/wordcount/AbstractJDKCompatible.java
+++ src/main/java/de/heidelberg/pvs/container_bench/wordcount/AbstractJDKCompatible.java
@@ -9,24 +9,32 @@ public abstract class AbstractJDKCompatible extends AbstractWordcountBenchmark<M
 	@Param
 	public ImplEnum impl;
 
+	private static final net.openhft.koloboke.collect.map.hash.HashObjObjMapFactory<Object, Object> KOLOBOKEQ = new net.openhft.koloboke.collect.impl.hash.QHashParallelKVObjObjMapFactoryImpl<>();
+
 	public static enum ImplEnum {
 		JDK_O2O_HASH(java.util.HashMap::new), //
 		JDK_O2O_LINKEDHASH(java.util.LinkedHashMap::new), //
 		JDK_O2O_TREE(java.util.TreeMap::new), //
 		FASTUTIL_O2O_HASH(it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap::new), //
+		FASTUTIL_O2O_LINKEDHASH(it.unimi.dsi.fastutil.objects.Object2ObjectLinkedOpenHashMap::new), //
 		FASTUTIL_O2O_AVL(it.unimi.dsi.fastutil.objects.Object2ObjectAVLTreeMap::new), //
 		FASTUTIL_O2O_RB(it.unimi.dsi.fastutil.objects.Object2ObjectRBTreeMap::new), //
 		FASTUTIL_O2I_HASH(it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap::new), //
+		FASTUTIL_O2I_LINKEDHASH(it.unimi.dsi.fastutil.objects.Object2IntLinkedOpenHashMap::new), //
 		FASTUTIL_O2I_AVL(it.unimi.dsi.fastutil.objects.Object2IntAVLTreeMap::new), //
 		FASTUTIL_O2I_RB(it.unimi.dsi.fastutil.objects.Object2IntRBTreeMap::new), //
 		TROVE_O2O_HASH(gnu.trove.map.hash.THashMap::new), //
 		KOLOBOKE_O2O_HASH(net.openhft.koloboke.collect.map.hash.HashObjObjMaps::newMutableMap), //
 		KOLOBOKE_O2I_HASH(net.openhft.koloboke.collect.map.hash.HashObjIntMaps::newMutableMap), //
+		KOLOBOKE_O2O_QHASH(() -> KOLOBOKEQ.newMutableMap()), //
+		KOLOBOKE_O2I_QHASH(() -> KOLOBOKEQ.newMutableMap()), //
 		ECLIPSE_O2O_HASH(org.eclipse.collections.impl.map.mutable.UnifiedMap::new), //
 		ECLIPSE_O2O_TREE(org.eclipse.collections.impl.map.sorted.mutable.TreeSortedMap::new), //
 		APACHE_O2O_HASH(org.apache.commons.collections4.map.HashedMap::new), //
 		APACHE_O2O_LINKEDHASH(org.apache.commons.collections4.map.LinkedMap::new), //
 		MAHOUT_O2O_HASH(org.apache.mahout.math.map.OpenHashMap::new), //
+		JAVOLUTION_HASH(javolution.util.FastMap::new), //
+		JAVOLUTION_SORTED(javolution.util.FastSortedMap::new), //
 		// These will be very slow:
 		FASTUTIL_O2O_ARRAY(it.unimi.dsi.fastutil.objects.Object2ObjectArrayMap::new), //
 		FASTUTIL_O2I_ARRAY(it.unimi.dsi.fastutil.objects.Object2IntArrayMap::new), //
