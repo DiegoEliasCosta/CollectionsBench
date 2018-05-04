4d3b191c9e77d768a58adb69a7e71d22dd2c215b
Erich Schubert <schubert@informatik.uni-heidelberg.de>
2018-05-04 14:21:56 +0200

    Add Mahout. Increase duration again.

---
+++ src/main/java/de/heidelberg/pvs/container_bench/wordcount/MahoutIntegerAddTo.java
@@ -0,0 +1,24 @@
+package de.heidelberg.pvs.container_bench.wordcount;
+
+import org.apache.mahout.math.map.OpenObjectIntHashMap;
+import org.openjdk.jmh.annotations.Param;
+
+public class MahoutIntegerAddTo extends AbstractWordcountBenchmark<OpenObjectIntHashMap<Object>> {
+	@Param({ "MAHOUT_O2I_HASH" })
+	public String impl;
+
+	@Override
+	protected OpenObjectIntHashMap<Object> makeMap() {
+		return new OpenObjectIntHashMap<Object>();
+	}
+
+	@Override
+	protected int size(OpenObjectIntHashMap<Object> map) {
+		return map.size();
+	}
+
+	@Override
+	protected void count(OpenObjectIntHashMap<Object> map, Object object) {
+		map.adjustOrPutValue(object, 1, 1);
+	}
+}
