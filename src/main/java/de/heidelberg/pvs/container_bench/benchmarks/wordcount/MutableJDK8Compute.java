b25a4492037cb1619ce946e54557391133c0fc30
Erich Schubert <schubert@informatik.uni-heidelberg.de>
2018-09-06 10:35:41 +0200

    Experiment with mutable integers for word count.

---
+++ src/main/java/de/heidelberg/pvs/container_bench/benchmarks/wordcount/MutableJDK8Compute.java
@@ -0,0 +1,30 @@
+package de.heidelberg.pvs.container_bench.benchmarks.wordcount;
+
+import java.util.Map;
+
+import org.openjdk.jmh.annotations.Param;
+
+import de.heidelberg.pvs.container_bench.factories.JDKMap2ObjFact;
+
+public class MutableJDK8Compute extends AbstractWordcountBenchmark<Map<Object, MutableInteger>> {
+	@Param
+	public JDKMap2ObjFact impl;
+
+	@SuppressWarnings("unchecked")
+	@Override
+	protected Map<Object, MutableInteger> makeMap() {
+		return (Map<Object, MutableInteger>) (Map<?, ?>) impl.maker.get();
+	}
+
+	@Override
+	protected void count(Map<Object, MutableInteger> map, String object) {
+		map.compute(object, (key, value) -> {
+			if (value == null) {
+				value = new MutableInteger(1);
+			} else {
+				value.v += 1;
+			}
+			return value;
+		});
+	}
+}
---
+++ src/main/java/de/heidelberg/pvs/container_bench/benchmarks/wordcount/MutableJDKGetPut.java
@@ -0,0 +1,28 @@
+package de.heidelberg.pvs.container_bench.benchmarks.wordcount;
+
+import java.util.Map;
+
+import org.openjdk.jmh.annotations.Param;
+
+import de.heidelberg.pvs.container_bench.factories.JDKMap2ObjFact;
+
+public class MutableJDKGetPut extends AbstractWordcountBenchmark<Map<Object, MutableInteger>> {
+	@Param
+	public JDKMap2ObjFact impl;
+
+	@SuppressWarnings("unchecked")
+	@Override
+	protected Map<Object, MutableInteger> makeMap() {
+		return (Map<Object, MutableInteger>) (Map<?, ?>) impl.maker.get();
+	}
+
+	@Override
+	protected void count(Map<Object, MutableInteger> map, String object) {
+		final MutableInteger old = map.get(object);
+		if (old != null) {
+			old.v++;
+		} else {
+			map.put(object, new MutableInteger(1));
+		}
+	}
+}
