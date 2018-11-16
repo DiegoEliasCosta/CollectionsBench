3145dfc21763f5b45b60ac2eb1058db099cf73c2
DiegoEliasCosta <diegoelias1@gmail.com>
2018-11-16 18:21:38 +0100

    Adding the benchmark annotation

--- src/main/java/de/heidelberg/pvs/container_bench/benchmarks/streaming/SourceSplittingCostExample.java
+++ src/main/java/de/heidelberg/pvs/container_bench/benchmarks/streaming/SourceSplittingCostExample.java
@@ -3,6 +3,7 @@ package de.heidelberg.pvs.container_bench.benchmarks.streaming;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 
+import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Measurement;
@@ -40,6 +41,7 @@ public class SourceSplittingCostExample {
 	 * This benchmark use iterative generator and should run considerably slower.
 	 * @param bh
 	 */
+	@Benchmark
 	public void sumWithIterativeGenerator(Blackhole bh) {
 		bh.consume(IntStream.iterate(0, i -> i + 1).limit(n).sum());
 	}
@@ -48,6 +50,7 @@ public class SourceSplittingCostExample {
 	 * This benchmark use stateless generator and should yield substantially 
 	 * faster results
 	 */
+	@Benchmark
 	public void sumWithStatelessGenerator(Blackhole bh) {
 		bh.consume(IntStream.range(0, n).sum());
 	}
